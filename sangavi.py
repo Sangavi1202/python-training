dccdae3f8916cc36c233ddcdc3f18aca675d4aa3
Sangavi1202 <sangavi22td0714@svcet.ac.in>
2025-01-30 11:03:44 +0530

    Add files via upload

---
+++ sangavi.py
@@ -0,0 +1,7 @@
+idlies=int(input("how many idlies are u ate:"))
+if idlies<=2:
+    print("hungry")
+elif idlies>=3:
+    print("sleep")
+else:
+ print("happy") 
---
+++ sangavi2.py
@@ -0,0 +1,3 @@
+l=["cake","apple","banana"]
+l.sort(reverse=True)
+print(l)
